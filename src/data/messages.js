5b63ff028d1aa6458f48872dbf450d179c7876e5
Tudor Tocan <tudor.tocan@gmail.com>
2021-07-08 18:51:30 +0100

    add seed script and functionality

--- src/data/messages.js
+++ src/data/messages.js
@@ -4,25 +4,27 @@ const messages = [
     email: "johnsmith@email.com",
     userMessage: "I`m John Smith and this is my test message",
     newsletterCheck: "on",
-    date: "Wednesday, July 7th 2021, 10:07:28 am",
+    date: "Wednesday, July 4th 2021",
   },
   {
     fullName: "Anne Smith",
     email: "annesmith@email.com",
     userMessage: "I`m Anne Smith and this is my test message",
     newsletterCheck: "on",
-    date: "Wednesday, July 7th 2021, 10:08:28 am",
+    date: "Wednesday, July 5th 2021",
   },
   {
     fullName: "Peter Smith",
     email: "petersmith@email.com",
     userMessage: "I`m Peter Smith and this is my test message",
-    date: "Wednesday, July 7th 2021, 10:09:28 am",
+    date: "Wednesday, July 6th 2021",
   },
   {
     fullName: "Dennis Smith",
     email: "dennissmith@email.com",
     userMessage: "I`m Dennis Smith and this is my test message",
-    date: "Wednesday, July 7th 2021, 10:07:28 am",
+    date: "Wednesday, July 7th 2021",
   },
 ];
+
+module.exports = messages;
---
+++ src/data/seed.js
@@ -0,0 +1,35 @@
+// importing mongoose
+const mongoose = require("mongoose");
+
+// importing model and seed data
+const { Message } = require("../models");
+const messagesData = require("./messages");
+
+// creating db options, name and url
+const dbOptions = { useNewUrlParser: true, useUnifiedTopology: true };
+
+const DB_NAME = "tastyBakeryDb";
+
+const DB_URL = process.env.MONGODB_URI || `mongodb://localhost/${DB_NAME}`;
+
+// connect to database
+mongoose.connect(DB_URL, dbOptions);
+
+// initializing seed
+const init = async () => {
+  try {
+    // awaits for Messages to be inserted into the db and it prints the data.
+    const messages = await Message.insertMany(messagesData);
+
+    console.log(messages);
+
+    // after the file was seeded, the connection is going to be closed
+    mongoose.connection.close();
+
+    // If there`s an error, the message is going to be printed.
+  } catch (error) {
+    console.info(error.message);
+  }
+};
+
+init();
